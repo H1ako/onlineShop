4f60d03c1f28c095cf9da979a7fb8fbc5dd98ece
H1ako <nikita-sobolev-wd@yandex.ru>
2022-06-09 16:49:47 +0400

    made header on the home page

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,11 +1,15 @@
 import './Search.scss'
 
+import { useState } from 'react'
+
 function Search() {
+  const [query, setQuery] = useState("")
+
   return (
     <input
         type="search"
-        // value={searchQuery}
-        // onChange={e => setSearchQuery(e.target.value)}
+        value={query}
+        onChange={e => setQuery(e.target.value)}
         name='search' 
         className='search'
         placeholder='Search'
