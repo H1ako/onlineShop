f7f5af4bb2ad09c5073e416d2d159ff6a73330be
H1ako <nikita-sobolev-wd@yandex.ru>
2022-06-11 12:12:04 +0400

    sldier v1

--- frontend/src/components/HorizontalSlider/HorizontalSlider.js
+++ frontend/src/components/HorizontalSlider/HorizontalSlider.js
@@ -1,19 +1,26 @@
+// styles
 import './HorizontalSlider.scss'
-
-import { useState } from 'react'
+// global
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
-import { ChevronLeftIcon, ChevronRightIcon, PauseIcon, PlayIcon, ArrowsExpandIcon, XIcon } from '@heroicons/react/outline'
+// icons
+import { ChevronLeftIcon, ChevronRightIcon, PauseIcon, PlayIcon, ArrowsExpandIcon } from '@heroicons/react/outline'
+// components
 import Lightbox from '../Lightbox/Lightbox'
 
 function HorizontalSlider({
-  withLinks=false,
-  pictures
+  withLinks=false, // should img be with a <a> wrapper or not
+  pictures // list of pictures with .image and .product
 }) {
 
   const [isChanging, setIsChanging] = useState(true)
   const [currentPictureId, setCurrentPictureId] = useState(0)
   const [isPictureExpanded, setIsPictureExpanded] = useState(false)
+  const slides = pictures.map(picture => (
+    <Link key={picture.product?.id} to={`/products/${picture.product?.id}`}>
+      <img src={picture.image} alt='' />
+    </Link>
+  ))
 
   const openLightbox = () => {
     setIsChanging(false)
@@ -25,6 +32,7 @@ function HorizontalSlider({
     setIsPictureExpanded(false)
   }
 
+  // change main picture
   const changeCurrentPicture = move => {
     let nextPictureNumber = currentPictureId + move
 
@@ -34,31 +42,35 @@ function HorizontalSlider({
     else if (nextPictureNumber <= -1) {
       nextPictureNumber = pictures.length - 1
     }
-    console.log(nextPictureNumber)
+    console.log(currentPictureId)
     setCurrentPictureId(nextPictureNumber)
   }
+
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPictureId(currentId => currentId === pictures.length - 1 ? 0 : currentId + 1)
+    }, 3000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+  }, [currentPictureId])
 
   return (
     <div className="horizontal-slider">
       <ul className="horizontal-slider__pictures">
-        {
-        withLinks ?
-          pictures.map(picture => (
-            <li key={picture.product?.id}>
-              <Link to={`/products/${picture.product?.id}`}>
-                <img src={picture.image} alt='' />
-              </Link>
-              <img src={picture.image} alt='' />
-            </li>
-          ))
-        :
-          pictures.map(picture => (
-            <li key={picture.product?.id}>
-              <img src={picture.image} alt='' />
-            </li>
-          ))
-        }
-        
+        <li className="pictures__prev">
+          {/* {slides[]} */}
+        </li>
+        <li className="pictures__current">
+          {slides[currentPictureId]}
+        </li>
+        <li className="pictures__next">
+
+        </li>
       </ul>
       <div className="horizontal-slider__tools">
         <ChevronLeftIcon className='tools__icon left-arrow' onClick={() => changeCurrentPicture(-1)} />
@@ -83,7 +95,6 @@ function HorizontalSlider({
         isPictureExpanded &&
         <Lightbox currentPicture={pictures[currentPictureId]} closeLightbox={closeLightbox} changeCurrentPicture={changeCurrentPicture} />
       }
-      
     </div>
   );
 }
--- frontend/src/components/Lightbox/Lightbox.js
+++ frontend/src/components/Lightbox/Lightbox.js
@@ -1,9 +1,8 @@
 import './Lightbox.scss'
 
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import { ChevronLeftIcon, ChevronRightIcon, PauseIcon, PlayIcon, ArrowsExpandIcon, XIcon, ExternalLinkIcon } from '@heroicons/react/outline'
+import { ChevronLeftIcon, ChevronRightIcon, XIcon, ExternalLinkIcon } from '@heroicons/react/outline'
 
 function Lightbox({
     changeCurrentPicture,
@@ -11,26 +10,34 @@ function Lightbox({
     currentPicture
 }) {
 
+  if (currentPicture.image == null) return (<></>)
+
   return (
     <div className="lightbox-area">
         <div className="lightbox-area__lightbox">
             <div className="lightbox__btns">
-                <ChevronLeftIcon className='btns__icon left-arrow' onClick={() => changeCurrentPicture(-1)} />
-                <ChevronRightIcon className='btns__icon right-arrow' onClick={() => changeCurrentPicture(1)} />
+                { changeCurrentPicture &&
+                    <>
+                    <ChevronLeftIcon className='btns__icon left-arrow' onClick={() => changeCurrentPicture(-1)} />
+                    <ChevronRightIcon className='btns__icon right-arrow' onClick={() => changeCurrentPicture(1)} />
+                    </>
+                }
                 <Link to={`/products/${currentPicture.product?.id}`} >
                     <ExternalLinkIcon className='btns__icon link' />
                 </Link>
                 <XIcon className='btns__icon close' onClick={closeLightbox} />
             </div>
             <div className="lightbox__picture">
-                <div className="picture__arrows">
-                    <div className="arrows__left-area" onClick={() => changeCurrentPicture(-1)}>
-                        <ChevronLeftIcon className='arrows__icon left-arrow' />
+                {changeCurrentPicture &&
+                    <div className="picture__arrows">
+                        <div className="arrows__left-area" onClick={() => changeCurrentPicture(-1)}>
+                            <ChevronLeftIcon className='arrows__icon left-arrow' />
+                        </div>
+                        <div className="arrows__right-area" onClick={() => changeCurrentPicture(1)}>
+                            <ChevronRightIcon className='arrows__icon right-arrow' />
+                        </div>          
                     </div>
-                    <div className="arrows__right-area" onClick={() => changeCurrentPicture(1)}>
-                        <ChevronRightIcon className='arrows__icon right-arrow' />
-                    </div>          
-                </div>
+                }
                 <img src={currentPicture.image} alt='' />
             </div>
         </div>
